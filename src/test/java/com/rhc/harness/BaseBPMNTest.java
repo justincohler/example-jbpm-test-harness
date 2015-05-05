8c2d6763dba26a597ba77f95867b8e9315e35788
Justin Cohler <justincohler@gmail.com>
2015-05-05 18:19:41 -0400

    updated formatting to google style format

--- src/test/java/com/rhc/harness/BaseBPMNTest.java
+++ src/test/java/com/rhc/harness/BaseBPMNTest.java
@@ -15,92 +15,100 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class constructs a ksession for the given test resources (bpmn2 classpath resources)
- * for use in each BPMN test case.
+ * This class constructs a ksession for the given test resources (bpmn2 classpath resources) for use
+ * in each BPMN test case.
  * 
  * @author jcohler
  * 
  */
 public class BaseBPMNTest extends JbpmJUnitBaseTestCase {
 
-	private static final Logger logger = LoggerFactory.getLogger(BaseBPMNTest.class);
-	
-	protected static final String TESTSUITE_ACTOR = "test";
-	private static final String HUMAN_TASK = "Human Task";
-	private static final String REST_TASK = "Rest";
+  private static final Logger logger = LoggerFactory.getLogger(BaseBPMNTest.class);
 
-	/**
-	 * This field can be used by all tests which extend this class to
-	 * instantiate processes, complete tasks, etc.
-	 * 
-	 */
-	protected KieSession ksession;
+  protected static final String TESTSUITE_ACTOR = "test";
+  private static final String HUMAN_TASK = "Human Task";
+  private static final String REST_TASK = "Rest";
 
-	/**
-	 * This field is used for reference in each test case to query process
-	 * variables, query tasks, etc.
-	 * 
-	 */
-	protected ProcessInstance processInstance;
+  /**
+   * This field can be used by all tests which extend this class to instantiate processes, complete
+   * tasks, etc.
+   * 
+   */
+  protected KieSession ksession;
 
-	/**
-	 * This list contains all of the bpmn2 classpath resources used in an
-	 * individual test case, and creates the runtime engine for each test.
-	 * 
-	 * e.g. ["myFirstProcess.bpmn2", "mySecondProcess.bpmn2"]
-	 * 
-	 */
-	protected List<String> testResources = new ArrayList<String>();
-	
-	/**
-	 * This map contains the process variables within each particular process
-	 * 
-	 */
-	protected Map<String, Object> processVars = new HashMap<String, Object>();
+  /**
+   * This field is used for reference in each test case to query process variables, query tasks,
+   * etc.
+   * 
+   */
+  protected ProcessInstance processInstance;
 
-	/**
-	 * This method is run before each individual BPMN test to set a new knowledge session
-	 * with the resources required for the test to run successfully.
-	 * 
-	 */
-	@Before
-	public void createKnowledgeSession() {
-		logger.info("Setting up knowledge session for resources: " + testResources.toString());
-		createRuntimeManager((String[]) testResources.toArray());
-		ksession = getRuntimeEngine().getKieSession();
-		ksession.getWorkItemManager().registerWorkItemHandler(HUMAN_TASK, getTestWorkItemHandler());
-		ksession.getWorkItemManager().registerWorkItemHandler(REST_TASK, getTestWorkItemHandler());
-		/*
-		 * TODO: Register additional Work Item Handlers needed for the test suite here
-		 */
-		processVars.clear();
-		logger.info("Knowledge session created successfully.");
-	}
-	
-	/**
-	 * This method loops through the ksession for outstanding work items with the given 
-	 * name and completes the work item with the given data outputs.
-	 * 
-	 * @param itemName
-	 * @param itemOutput
-	 */
-	public void completeWorkItem(String itemName, Map<String, Object> itemOutput) {
-		for (WorkItem item : getTestWorkItemHandler().getWorkItems()) {
-			if (((String) item.getParameter("NodeName")).equalsIgnoreCase(itemName)) {
-				ksession.getWorkItemManager().completeWorkItem(item.getId(), itemOutput);
-			}
-		}
-	}
-	
-	/**
-	 * This method sets this base class with the resource names to be injected in
-	 * the ksession for each individual test.
-	 * 
-	 * @param resources
-	 */
-	public void setTestResources(String... resources) {
-		testResources.clear();
-		testResources.addAll(Arrays.asList(resources));
-	}
+  /**
+   * This list contains all of the bpmn2 classpath resources used in an individual test case, and
+   * creates the runtime engine for each test.
+   * 
+   * e.g. ["myFirstProcess.bpmn2", "mySecondProcess.bpmn2"]
+   * 
+   */
+  protected List<String> testResources = new ArrayList<String>();
+
+  /**
+   * This map contains the process variables within each particular process
+   * 
+   */
+  protected Map<String, Object> processVars = new HashMap<String, Object>();
+
+  /**
+   * This method is run before each individual BPMN test to set a new knowledge session with the
+   * resources required for the test to run successfully.
+   * 
+   */
+  @Before
+  public void createKnowledgeSession() {
+    logger.info("Setting up knowledge session for resources: " + testResources.toString());
+    createRuntimeManager((String[]) testResources.toArray());
+    ksession = getRuntimeEngine().getKieSession();
+    ksession.getWorkItemManager().registerWorkItemHandler(HUMAN_TASK, getTestWorkItemHandler());
+    ksession.getWorkItemManager().registerWorkItemHandler(REST_TASK, getTestWorkItemHandler());
+    /*
+     * TODO: Register additional Work Item Handlers needed for the test suite here
+     */
+    processVars.clear();
+    logger.info("Knowledge session created successfully.");
+  }
+
+  /**
+   * This method loops through the ksession for outstanding work items with the given name and
+   * completes the work item with the given data outputs.
+   * 
+   * @param itemName
+   * @param itemOutput
+   * @throws NoSuchFieldException
+   */
+  public void completeWorkItem(String itemName, Map<String, Object> itemOutput)
+      throws NoSuchFieldException {
+    boolean foundItem = false;
+    for (WorkItem item : getTestWorkItemHandler().getWorkItems()) {
+      if (((String) item.getParameter("NodeName")).equalsIgnoreCase(itemName)) {
+        foundItem = true;
+        ksession.getWorkItemManager().completeWorkItem(item.getId(), itemOutput);
+      }
+    }
+    if (!foundItem) {
+      throw new NoSuchFieldException(
+          "The following Work Item was not found or could not be completed: " + itemName);
+    }
+  }
+
+  /**
+   * This method sets this base class with the resource names to be injected in the ksession for
+   * each individual test.
+   * 
+   * @param resources
+   */
+  public void setTestResources(String... resources) {
+    testResources.clear();
+    testResources.addAll(Arrays.asList(resources));
+  }
 
 }
--- src/test/java/com/rhc/tests/BudgetProcessTest.java
+++ src/test/java/com/rhc/tests/BudgetProcessTest.java
@@ -11,61 +11,67 @@ import org.slf4j.LoggerFactory;
 import com.rhc.harness.BaseBPMNTest;
 
 /**
- * This is an example BPMN test class for a Budget Process that includes 
- * gateways, timers, and user tasks.
+ * This is an example BPMN test class for a Budget Process that includes gateways, timers, and user
+ * tasks.
  * 
  * @author jcohler
- *
+ * 
  */
 public class BudgetProcessTest extends BaseBPMNTest {
-	
-	private static final Logger logger = LoggerFactory.getLogger(BudgetProcessTest.class);
-	private static final String P_BUDGET = "budgeting.budgetProcess";
-	
-	@Test
-	public void should_escalate_exceeded_budget() {
-		setTestResources("budgetProcess.bpmn2");
-		processVars.put("user1", TESTSUITE_ACTOR);
-		processVars.put("spending", 10001);
-		
-		processInstance = ksession.startProcess(P_BUDGET, processVars);
-		assertProcessInstanceActive(processInstance.getId(), ksession);
-	
 
-		WorkflowProcessInstanceImpl workflowInstance = (WorkflowProcessInstanceImpl) processInstance;
-		assertEquals(12345, workflowInstance.getVariable("spending"));
-		
-		assertNodeTriggered(processInstance.getId(), "Determine Budget Remaining");
-		assertNodeActive(processInstance.getId(), ksession, "Budget Exceeded");
-		
-		completeWorkItem("BudgetExceeded", null);
-		assertProcessInstanceCompleted(processInstance.getId());
-	}
-	
-	@Test
-	public void should_not_escalate_exceeded_budget() throws InterruptedException {
-		setTestResources("budgetProcess.bpmn2");
-		processVars.put("user1", TESTSUITE_ACTOR);
-		processVars.put("spending", 9999);
-		
-		processInstance = ksession.startProcess(P_BUDGET, processVars);
-		assertProcessInstanceActive(processInstance.getId(), ksession);
-	
+  private static final Logger logger = LoggerFactory.getLogger(BudgetProcessTest.class);
+  private static final String P_BUDGET = "budgeting.budgetProcess";
 
-		WorkflowProcessInstanceImpl workflowInstance = (WorkflowProcessInstanceImpl) processInstance;
-		assertEquals(12345, workflowInstance.getVariable("spending"));
-		
-		assertNodeTriggered(processInstance.getId(), "Determine Budget Remaining");
-		assertNodeActive(processInstance.getId(), ksession, "Review Budget Remaining");
-		
-		Map<String, Object> itemOutput = new HashMap<String, Object>();
-		itemOutput.put("timerDuration", "2s");
-		completeWorkItem("Review Budget Remaining", itemOutput);
-		
-		// sleep just a bit longer than the timer to be safe
-		logger.info("Waiting 2 seconds for timer event to fire...");
-		Thread.sleep(2200);
-		
-		assertProcessInstanceCompleted(processInstance.getId());
-	}
+  @Test
+  public void should_escalate_exceeded_budget() {
+    setTestResources("budgetProcess.bpmn2");
+    processVars.put("user1", TESTSUITE_ACTOR);
+    processVars.put("spending", 10001);
+
+    processInstance = ksession.startProcess(P_BUDGET, processVars);
+    assertProcessInstanceActive(processInstance.getId(), ksession);
+
+    WorkflowProcessInstanceImpl workflowInstance = (WorkflowProcessInstanceImpl) processInstance;
+    assertEquals(12345, workflowInstance.getVariable("spending"));
+
+    assertNodeTriggered(processInstance.getId(), "Determine Budget Remaining");
+    assertNodeActive(processInstance.getId(), ksession, "Budget Exceeded");
+
+    try {
+      completeWorkItem("BudgetExceeded", null);
+    } catch (NoSuchFieldException e) {
+      fail(e.getMessage());
+    }
+    assertProcessInstanceCompleted(processInstance.getId());
+  }
+
+  @Test
+  public void should_not_escalate_exceeded_budget() throws InterruptedException {
+    setTestResources("budgetProcess.bpmn2");
+    processVars.put("user1", TESTSUITE_ACTOR);
+    processVars.put("spending", 9999);
+
+    processInstance = ksession.startProcess(P_BUDGET, processVars);
+    assertProcessInstanceActive(processInstance.getId(), ksession);
+
+    WorkflowProcessInstanceImpl workflowInstance = (WorkflowProcessInstanceImpl) processInstance;
+    assertEquals(12345, workflowInstance.getVariable("spending"));
+
+    assertNodeTriggered(processInstance.getId(), "Determine Budget Remaining");
+    assertNodeActive(processInstance.getId(), ksession, "Review Budget Remaining");
+
+    Map<String, Object> itemOutput = new HashMap<String, Object>();
+    itemOutput.put("timerDuration", "2s");
+    try {
+      completeWorkItem("Review Budget Remaining", itemOutput);
+    } catch (NoSuchFieldException e) {
+      fail(e.getMessage());
+    }
+
+    // sleep just a bit longer than the timer to be safe
+    logger.info("Waiting 2 seconds for timer event to fire...");
+    Thread.sleep(2200);
+
+    assertProcessInstanceCompleted(processInstance.getId());
+  }
 }
